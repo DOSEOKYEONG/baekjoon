121b732269366eef80a889854fe819a97bd63c5f
DOSEOKYEONG <dho88dho@naver.com>
2023-01-21 11:53:21 +0900

    p11654

---
+++ src/p11654.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class p11654 {
+    // https://www.acmicpc.net/problem/11654
+    // 아스키 코드
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.next().charAt(0);
+
+        System.out.println(a);
+    }
+}
