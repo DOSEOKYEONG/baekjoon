089050b20bc4c4fefb2bf8d27ebb7d1d76b90be4
DOSEOKYEONG <dho88dho@naver.com>
2023-01-26 23:17:35 +0900

    p2839

---
+++ src/p2839.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class p2839{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        if (num < 5){
+            if (num == 3){
+                System.out.println(1);
+            }else {
+                System.out.println(-1);
+            }
+            return;
+        }
+        int arr[] = new int[num + 1];
+        Arrays.fill(arr, 9999);
+        arr[3] = 1;
+        arr[5] = 1;
+        for (int i = 6; i < arr.length; i++){
+            arr[i] = Math.min(arr[i - 3] + 1, arr[i - 5] + 1);
+        }
+        if (arr[num] > 9999){
+            System.out.println(-1);
+        }else {
+            System.out.println(arr[num]);
+        }
+    }
+}
